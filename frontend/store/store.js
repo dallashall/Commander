0c2f2e240b479328b4692ef5a347a623ce636b0b
Dallas Hall <dallas.hall@gmail.com>
2017-03-17 00:31:49 -0700

    Removes devtools

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,8 +1,11 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default (preloadedState) => (
-  createStore(RootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk)))
+  createStore(RootReducer, preloadedState, applyMiddleware(thunk))
 );
+// export default (preloadedState) => (
+//   createStore(RootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk)))
+// );
