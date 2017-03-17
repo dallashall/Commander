111c39bfe3c0b711b50c92794e0993bae87db90c
Dallas Hall <dallas.hall@gmail.com>
2017-03-17 14:49:11 -0700

    Prep for Heroku

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,9 +3,9 @@ import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-// export default (preloadedState) => (
-//   createStore(RootReducer, preloadedState, applyMiddleware(thunk))
-// );
 export default (preloadedState) => (
-  createStore(RootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk)))
+  createStore(RootReducer, preloadedState, applyMiddleware(thunk))
 );
+// export default (preloadedState) => (
+//   createStore(RootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk)))
+// );
