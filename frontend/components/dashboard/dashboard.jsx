c430cf53a5c4556385916bc21edc4e24f450459e
Dallas Hall <dallas.hall@gmail.com>
2017-05-12 07:42:09 -0700

    More experiments with animations

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -7,26 +7,67 @@ import ProjectsContainer from './projects/projects_container';
 import { hashHistory } from 'react-router';
 import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 
-export default ({children}) => {
-  return (
-    <div className={"container full-height"}>
-      <nav className={"sidebar"}>
-        <div className={"logo"}>
-          <h2 className="pointer" onClick={() => hashHistory.push('/dashboard')}><small>task</small>Commander</h2>
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animationCSS: ""
+    }
+    this.animateEnter = this.animateEnter.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let { children } = this.props;
+    let nextChildren = nextProps.children;
+    if (children.props.route.component.displayName !== nextChildren.props.route.component.displayName) {
+      console.log("Different.");
+      this.animateEnter();
+    }
+  }
+
+  shouldComponentUpdate() {
+    return setTimeout(function() {
+      return true;
+    }, 300);
+  }
+
+  animateEnter() {
+    this.setState({ animationCSS: "main-slide-leave" });
+    this.setState({ animationCSS: "main-slide-leave main-slide-leave-active" });
+    setTimeout(() => {
+      this.setState({ animationCSS: "main-slide-enter" });
+      this.setState({ animationCSS: "main-slide-enter main-slide-enter-active" });
+      setTimeout(() => {
+        this.setState({ animationCSS: "" });
+      }, 300);
+    }, 300);
+  }
+
+  render() {
+    let { children } = this.props;
+    return (
+      <div className={"container full-height"}>
+        <nav className={"sidebar"}>
+          <div className={"logo"}>
+            <h2 className="pointer" onClick={() => hashHistory.push('/dashboard')}><small>task</small>Commander</h2>
+          </div>
+          <MyTeamsContainer />
+          <TeamsContainer />
+          <TeamMemberContainer />
+          <ProjectsContainer />
+        </nav>
+        <div className="main-area">
+          <HeaderContainer />
+          <main>
+            <p>{this.state.animationCSS}</p> 
+              {
+                children
+              }
+          </main>
         </div>
-        <MyTeamsContainer />
-        <TeamsContainer />
-        <TeamMemberContainer />
-        <ProjectsContainer />
-      </nav>
-      <div className="main-area">
-        <HeaderContainer />
-        <main>
-            {
-              children
-            }
-        </main>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Dashboard;
