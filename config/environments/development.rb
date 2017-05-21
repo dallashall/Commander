263fdc9960faac491501e2d74852f2a32fc626d2
Dallas Hall <dallas.hall@gmail.com>
2017-05-20 20:25:20 -0700

    Guard settings to development only..

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  Rails.application.configure do
+    config.middleware.insert_after(ActionDispatch::Static, Rack::LiveReload)
+  end
 end
