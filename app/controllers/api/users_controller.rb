31debb82bd23fee599478df6994833d307853b78
Dallas Hall <dallas.hall@gmail.com>
2017-05-25 23:12:19 -0700

    typo

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -22,11 +22,12 @@ class Api::UsersController < ApplicationController
     render '/api/tasks/index.json.jbuilder'
   end
 
+  API_KEY = ENV['mailgun_api_key']
+  DOMAIN = ENV['domain']
+  API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/#{DOMAIN}"
+  email = new_member_params[:email]
+  
   def new_member
-    API_KEY = ENV['mailgun_api_key']
-    DOMAIN = ENV['domain']
-    API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/#{DOMAIN}"
-    email = new_member_params[:email]
     RestClient.post API_URL+"/messages",
       :from => email,
       :to => email,
