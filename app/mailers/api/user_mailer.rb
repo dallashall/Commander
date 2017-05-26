10426ffdc5c1f2c7e2140ee240203ee6e5d68040
Dallas Hall <dallas.hall@gmail.com>
2017-05-25 22:36:28 -0700

    reconfigure mailer

--- app/mailers/api/user_mailer.rb
+++ app/mailers/api/user_mailer.rb
@@ -1,7 +1,13 @@
 class Api::UserMailer < ApplicationMailer
+
   def add_member_to_team(email, team)
     @email = email
     @team = team
-    mail(to: @email, subject: "I'd like for you to join my team, #{@team.name}, on TaskCommander.")
+    mg_client = Mailgun::Client.new ENV['mailgun_api_key']
+    message_params = {from: ENV['email'],
+                      to: @email,
+                      subject: 'Sample Mail using Mailgun API',
+                      text: "I'd like for you to join my team, #{@team.name}, on TaskCommander."}
+    mg_client.send_message ENV['domain'], message_params
   end
 end
