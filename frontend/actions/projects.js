482f909f94e8d98229e44b4e052cd655d3413b3a
Dallas Hall <dallas.hall@gmail.com>
2017-12-14 22:41:15 -0800

    Complete projects actions

---
+++ frontend/actions/projects.js
@@ -0,0 +1,46 @@
+import { 
+  getToApi,
+  deleteToApi,
+  patchToApi,
+  postToApi,
+} from '../util/api_util';
+ 
+export const RECEIVE_SINGLE_PROJECT = 'RECEIVE_SINGLE_PROJECT';
+export const RECEIVE_PROJECTS = 'RECEIVE_PROJECTS';
+export const REMOVE_PROJECT = 'REMOVE_PROJECT';
+export const RECEIVE_PROJECT_ERRORS = 'RECEIVE_PROJECT_ERRORS';
+
+const action = (type, payload) => ({
+  type,
+  payload,
+});
+
+export const createProject = formProject => dispatch => (
+  postToApi('/projects', formProject)
+    .then(payload => dispatch(action(RECEIVE_SINGLE_PROJECT, payload)))
+    .catch(errors => dispatch(action(RECEIVE_PROJECT_ERRORS, errors)))
+);
+
+export const updateProject = formProject => dispatch => (
+  patchToApi(`/projects/${formProject.id}`, formProject)
+    .then(payload => dispatch(action(RECEIVE_SINGLE_PROJECT, payload)))
+    .catch(errors => dispatch(action(RECEIVE_PROJECT_ERRORS, errors)))
+);
+
+export const fetchProject = projectId => dispatch => (
+  getToApi(`/projects/${projectId}`)
+    .then(payload => dispatch(action(RECEIVE_SINGLE_PROJECT, payload)))
+    .catch(errors => dispatch(action(RECEIVE_PROJECT_ERRORS, errors)))
+);
+
+export const fetchTeamProjects = teamId => dispatch => (
+  getToApi(`/teams/${teamId}/projects`)
+    .then(payload => dispatch(action(RECEIVE_PROJECTS, payload)))
+    .catch(errors => dispatch(action(RECEIVE_PROJECT_ERRORS, errors)))
+);
+
+export const deleteProject = projectId => dispatch => (
+  deleteToApi(`/projects/${projectId}`)
+    .then(payload => dispatch(action(REMOVE_PROJECT, payload)))
+    .catch(errors => dispatch(action(RECEIVE_PROJECT_ERRORS, errors)))
+);
