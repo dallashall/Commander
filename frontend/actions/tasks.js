b6d4b953abaff54ec2f79eeafc77004348d40701
Dallas Hall <dallas.hall@gmail.com>
2017-12-14 21:58:42 -0800

    add task actions

---
+++ frontend/actions/tasks.js
@@ -0,0 +1,61 @@
+import {
+  getToApi,
+  deleteToApi,
+  patchToApi,
+  postToApi,
+} from '../util/api_util';
+
+export const RECEIVE_SINGLE_TASK = 'RECEIVE_SINGLE_TASK';
+export const RECEIVE_ALL_TASKS = 'RECEIVE_ALL_TASKS';
+export const REMOVE_TASK = 'REMOVE_TASK';
+export const RECEIVE_TASK_ERRORS = 'RECEIVE_TASK_ERRORS';
+
+const receiveSingleTask = payload => ({
+  type: RECEIVE_SINGLE_TASK,
+  payload,
+});
+
+const receiveAllTasks = payload => ({
+  type: RECEIVE_ALL_TASKS,
+  payload,
+});
+
+const removeTask = payload => ({
+  type: REMOVE_TASK,
+  payload,
+});
+
+const receiveErrors = errors => ({
+  type: RECEIVE_TASK_ERRORS,
+  errors
+})
+
+export const createTask = formTask => dispatch => (
+  postToApi('/tasks', formTask)
+    .then(payload => dispatch(receiveSingleTask(payload)))
+    .catch(errors => dispatch(receiveErrors(errors)))
+);
+
+export const updateTask = formTask => dispatch => (
+  patctToApi(`/tasks/${formTask.id}`, formTask)
+    .then(payload => dispatch(receiveSingleTask(payload)))
+    .catch(errors => dispatch(receiveErrors(errors)))
+);
+
+export const fetchSingleTask = id => dispatch => (
+  getToApi(`/tasks/${id}`)
+    .then(payload => dispatch(receiveSingleTask(payload)))
+    .catch(errors => dispatch(receiveErrors(errors)))
+);
+
+export const fetchAllTasks = projectId => dispatch => (
+  getToApi(`/projects/${projectId}/tasks`)
+    .then(payload => dispatch(receiveAllTasks(payload)))
+    .catch(errors => dispatch(receiveErrors(errors)))
+);
+
+export const deleteTask = taskId => dispatch => (
+  getToApi(`/projects/${projectId}/tasks`)
+    .then(payload => dispatch(receiveAllTasks(payload)))
+    .catch(errors => dispatch(receiveErrors(errors)))
+);
